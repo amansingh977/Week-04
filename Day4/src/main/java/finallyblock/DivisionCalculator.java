930fd0cdbc01291d3dda7024fc0c9b76b0097f76
amansingh977 <amansinghrajawat09@gmail.com>
2025-02-14 16:35:55 +0530

    Aman Add. Finally block

---
+++ Day4/src/main/java/finallyblock/DivisionCalculator.java
@@ -0,0 +1,11 @@
+package finallyblock;
+
+public class DivisionCalculator {
+    public static int divide(int a, int b) {
+        try {
+            return a / b; // May throw ArithmeticException
+        } finally {
+            System.out.println("Operation completed");
+        }
+    }
+}
