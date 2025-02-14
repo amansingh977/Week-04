5670601154c546caf7e180473708b82fdd798bad
amansingh977 <amansinghrajawat09@gmail.com>
2025-02-14 16:34:39 +0530

    Aman Add. Try with resources

---
+++ Day4/src/main/java/trywithresources/MainApp.java
@@ -0,0 +1,10 @@
+package trywithresources;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String filePath = "info.txt"; // File to read
+        String firstLine = FileReaderUtil.readFirstLine(filePath);
+
+        System.out.println(firstLine);
+    }
+}
